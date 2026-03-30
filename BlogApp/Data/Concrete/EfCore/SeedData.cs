32043413e874be40d6406d659674818bb4723592
akadirkaratas <a.kadir.karatass@gmail.com>
2026-03-30 16:36:52 +0300

    db ye test datalari eklendi

---
+++ BlogApp/Data/Concrete/EfCore/SeedData.cs
@@ -0,0 +1,78 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApp.Data.Concrete.EfCore
+{
+    public static class SeedData
+    {
+        public static void TestVerileriniDoldur(IApplicationBuilder app)
+        {
+            var context = app.ApplicationServices.CreateScope().ServiceProvider.GetService<BlogContext>();
+
+            if(context != null)
+            {
+                if (context.Database.GetPendingMigrations().Any())
+                {
+                    context.Database.Migrate();
+                }
+
+                if (!context.Tags.Any())
+                {
+                    context.Tags.AddRange(
+                        new Entity.Tag{Text ="web programlama"},
+                        new Entity.Tag{Text ="backend"},
+                        new Entity.Tag{Text ="frontend"},
+                        new Entity.Tag{Text ="fullstack"},
+                        new Entity.Tag{Text ="php"}
+                    );
+                    context.SaveChanges();
+                }
+
+
+                if (!context.Users.Any())
+                {
+                    context.Users.AddRange(
+                        new Entity.User{UserName="kadirkaratas"},
+                        new Entity.User{UserName="mehmetali"}
+                    );
+                    context.SaveChanges();
+                }
+
+                if (!context.Posts.Any())
+                {
+                    context.Posts.AddRange(
+                        new Entity.Post{
+                            Title="Asp.net core",
+                            Content="Asp.net core dersleri",
+                            IsActive=true,
+                            PublishedOn=DateTime.Now.AddDays(-10),
+                            Tags=context.Tags.Take(3).ToList(),
+                            UserId=1
+                            
+                        },
+
+                        new Entity.Post{
+                            Title="Php",
+                            Content="Php core dersleri",
+                            IsActive=true,
+                            PublishedOn=DateTime.Now.AddDays(-20),
+                            Tags=context.Tags.Take(2).ToList(),
+                            UserId=1
+                            
+                        },
+
+                        new Entity.Post{
+                            Title="Django",
+                            Content="Django dersleri",
+                            IsActive=true,
+                            PublishedOn=DateTime.Now.AddDays(-5),
+                            Tags=context.Tags.Take(4).ToList(),
+                            UserId=2
+                            
+                        }
+                    );
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
