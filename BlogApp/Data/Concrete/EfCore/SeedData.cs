9356cad064386ffc90fb027998815e0ded887db6
akadirkaratas <a.kadir.karatass@gmail.com>
2026-04-08 17:32:13 +0300

    template calismasi basladi

--- BlogApp/Data/Concrete/EfCore/SeedData.cs
+++ BlogApp/Data/Concrete/EfCore/SeedData.cs
@@ -46,6 +46,7 @@ namespace BlogApp.Data.Concrete.EfCore
                             IsActive=true,
                             PublishedOn=DateTime.Now.AddDays(-10),
                             Tags=context.Tags.Take(3).ToList(),
+                            Image="1.jpg",
                             UserId=1
                             
                         },
@@ -56,6 +57,7 @@ namespace BlogApp.Data.Concrete.EfCore
                             IsActive=true,
                             PublishedOn=DateTime.Now.AddDays(-20),
                             Tags=context.Tags.Take(2).ToList(),
+                            Image="2.jpg",
                             UserId=1
                             
                         },
@@ -66,6 +68,7 @@ namespace BlogApp.Data.Concrete.EfCore
                             IsActive=true,
                             PublishedOn=DateTime.Now.AddDays(-5),
                             Tags=context.Tags.Take(4).ToList(),
+                            Image="3.jpg",
                             UserId=2
                             
                         }
