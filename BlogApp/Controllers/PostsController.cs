bf4992328f93ec18ac14a04145f861474d058714
akadirkaratas <a.kadir.karatass@gmail.com>
2026-04-15 01:56:30 +0300

    postlara details sayfasi eklendi

--- BlogApp/Controllers/PostsController.cs
+++ BlogApp/Controllers/PostsController.cs
@@ -3,6 +3,7 @@ using BlogApp.Data.Concrete.EfCore;
 using BlogApp.Entity;
 using BlogApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlogApp.Controllers
 {
@@ -23,6 +24,11 @@ namespace BlogApp.Controllers
                     Posts = _postRepository.Posts.ToList()
                 }
             );
+        }
+
+        public async Task<IActionResult> Details (int? id)
+        {
+            return View(await _postRepository.Posts.FirstOrDefaultAsync(p => p.PostId == id));
         }
     }
 }
