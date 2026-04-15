6d8c3e7f811c2dcfae5299500c417cef8b533246
akadirkaratas <a.kadir.karatass@gmail.com>
2026-04-15 17:29:04 +0300

    post detayında url post içeriğine göre geliyor

--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -21,6 +21,15 @@ app.UseStaticFiles();
 
 SeedData.TestVerileriniDoldur(app);
 
-app.MapDefaultControllerRoute();
+app.MapControllerRoute(
+   name: "post_details",
+   pattern: "posts/{url}",
+   defaults: new {controller = "Posts", action = "Details"}
+);
+
+app.MapControllerRoute(
+   name: "default",
+   pattern: "{controller=Home}/{action=Index}/{id?}"
+);
 
 app.Run();
