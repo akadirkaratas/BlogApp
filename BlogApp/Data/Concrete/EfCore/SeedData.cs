2ff1a45ffde8b25990d820c90cad1543211c4f20
akadirkaratas <a.kadir.karatass@gmail.com>
2026-04-17 03:05:03 +0300

    tag list in details eklendi

--- BlogApp/Data/Concrete/EfCore/SeedData.cs
+++ BlogApp/Data/Concrete/EfCore/SeedData.cs
@@ -1,3 +1,4 @@
+using BlogApp.Entity;
 using Microsoft.EntityFrameworkCore;
 
 namespace BlogApp.Data.Concrete.EfCore
@@ -18,11 +19,11 @@ namespace BlogApp.Data.Concrete.EfCore
                 if (!context.Tags.Any())
                 {
                     context.Tags.AddRange(
-                        new Entity.Tag{Text ="web programlama", Url="web-programlama"},
-                        new Entity.Tag{Text ="backend", Url="backend"},
-                        new Entity.Tag{Text ="frontend", Url="frontend"},
-                        new Entity.Tag{Text ="fullstack", Url="fullstack"},
-                        new Entity.Tag{Text ="php", Url="php"}
+                        new Entity.Tag{Text ="web programlama", Url="web-programlama", Color = TagColors.info},
+                        new Entity.Tag{Text ="backend", Url="backend", Color = TagColors.primary},
+                        new Entity.Tag{Text ="frontend", Url="frontend", Color = TagColors.success},
+                        new Entity.Tag{Text ="fullstack", Url="fullstack", Color = TagColors.secondary},
+                        new Entity.Tag{Text ="php", Url="php", Color = TagColors.danger}
                     );
                     context.SaveChanges();
                 }
